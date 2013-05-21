2aa1dee3f000e1cc409769b4d94b53ca27a6514f
Daniele Rosario <realskullbock@gmail.com>
2013-05-21 11:18:55 +0200

    Added Comments to the demo app

--- examples/media/zoo/applications/demo1/demo1.php
+++ examples/media/zoo/applications/demo1/demo1.php
@@ -1,3 +1,8 @@
 <?php
 
+/**
+ * Application Class. Here you can do advanced things, like
+ * adding new paths for the application, changing the way the 
+ * actions are dispatched etc.
+ */
 class Demo1Application extends Application {}
